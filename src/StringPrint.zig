be0bcd428bafe2939fbd7f91212e8a3e0fcef19d
Olive <89336634+OliveIsAWord@users.noreply.github.com>
2023-05-10 04:54:03 -0700

    may you long live in version control

--- src/StringPrint.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-data: []const u8,
-
-const Self = @This();
-
-pub fn format(self: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-    return writer.print("{s}", .{self.data});
-}
